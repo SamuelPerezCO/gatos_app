f73ffbda3ad86c09b464c3cb2d328c2a4edf26d5
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-07-03 17:01:41 -0500

    Recibiendo y redimensionando

--- src/main/java/com/mycompany/gatos_app/GatosService.java
+++ src/main/java/com/mycompany/gatos_app/GatosService.java
@@ -4,13 +4,34 @@
  */
 package com.mycompany.gatos_app;
 
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
 /**
  *
  * @author serna
  */
 public class GatosService {
     
-    public static void verGatos(){
-    
+    public static void verGatos() throws IOException{
+        //1. Vamos a traer los datos de la API
+        OkHttpClient client = new OkHttpClient();
+        
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get().build();
+        
+        Response response = client.newCall(request).execute();
+        
+        String elJson = response.body().string();
+        
+        //cortar los corchetes
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0 , elJson.length()-1);
+        
+        // Crear un objeto de la clase GSON
+        Gson gson = new Gson();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
     }
 }
